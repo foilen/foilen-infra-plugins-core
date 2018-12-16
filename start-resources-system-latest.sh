f736c63934b63ddb29dc1111126742c3a694d487
Simon Levesque <simon@foilen.com>
2018-12-16 15:26:30 -0500

    Add a script to test the resources locally

---
+++ start-resources-system-latest.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e 
+
+if [ $# -ne 1 ]
+  then
+    echo Usage: $0 file or folder to import;
+    exit 1;
+fi
+
+TO_IMPORT=$(readlink -f $1)
+
+RUN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $RUN_PATH
+
+# Prepare folders
+FOLDER_PLUGINS_JARS=$(pwd)/_plugins-jars
+mkdir -p $FOLDER_PLUGINS_JARS
+
+# Start resources
+docker run -ti \
+  --rm \
+  --env HOSTFS=/hostfs/ \
+  --env PLUGINS_JARS=/plugins \
+  --volume $FOLDER_PLUGINS_JARS:/plugins \
+  --volume $TO_IMPORT:/to_import \
+  --volume /etc:/hostfs/etc \
+  --volume /home:/hostfs/home \
+  --volume /usr/bin/docker:/usr/bin/docker \
+  --volume /usr/lib/x86_64-linux-gnu/libltdl.so.7.3.1:/usr/lib/x86_64-linux-gnu/libltdl.so.7 \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  foilen/foilen-infra-system-app-test-docker \
+  start-resources \
+  /to_import
