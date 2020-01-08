8a58ada9137ab0211f11cc6d6e4420d2d14adfd4
Simon Levesque <simon@foilen.com>
2020-01-08 07:06:20 -0500

    Update copyright

--- src/main/java/com/foilen/infra/resource/global/upgrader/V_2019090401_Refresh_InfraConfig.java
+++ src/main/java/com/foilen/infra/resource/global/upgrader/V_2019090401_Refresh_InfraConfig.java
@@ -1,7 +1,7 @@
 /*
     Foilen Infra Plugins Core
     https://github.com/foilen/foilen-infra-plugins-core
-    Copyright (c) 2018-2019 Foilen (http://foilen.com)
+    Copyright (c) 2018-2020 Foilen (http://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
